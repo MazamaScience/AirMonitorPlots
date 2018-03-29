af5d23c7269fa5c92fcd455f7b09d4cc397c4fe2
Spencer Pease <spencer.p.816@gmail.com>
2018-03-29 13:26:29 -0700

    Intialize file to handle handle data formatting functions

---
+++ R/formatData.R
@@ -0,0 +1,14 @@
+
+#' @title Check the Format of Data
+#'
+#' @param data data to interrogate
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+checkDataFormat <- function(data = NULL) {
+    
+    
+}
---
+++ localExamples/data_formatting.R
@@ -0,0 +1,7 @@
+############################
+# Data formatting examples #
+############################
+
+library(PWFSLSmoke)
+
+monitorData <- Northwest_Megafires 
