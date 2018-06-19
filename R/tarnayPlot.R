5b1a4a83c900a8833005b35117b70c7da0b0601c
Spencer Pease <spencer.p.816@gmail.com>
2018-06-19 12:50:57 -0700

    Update theme styling
    - adjust legend styling
    - add y-axis tick marks
    
    References issues #5, #2

--- R/tarnayPlot.R
+++ R/tarnayPlot.R
@@ -191,7 +191,7 @@ createTarnayPlot <- function(monitors,
     # TODO: combine AQI text into single scale
     # TODO: make legend scale with plot size
     scale_fill_manual(
-      name = "Daily Air Quality Index (24 hr AQI)",
+      name = "Daily Air Quality Index (24hr AQI)",
       values = aqiColors,
       labels = aqiNames,
       drop = FALSE,
@@ -235,6 +235,8 @@ createTarnayPlot <- function(monitors,
 
 theme_mazamaBar <- function(base_size = 11, base_family = "") {
 
+  half_line <- base_size/2
+
   # TODO: Create theme object that can be used across the package
   ggplot2::theme_minimal(
     base_size = base_size,
@@ -244,16 +246,20 @@ theme_mazamaBar <- function(base_size = 11, base_family = "") {
   ggplot2::theme(
     strip.background = element_rect(fill = "#E0E0E0"),
 
-    panel.background = element_rect(color = "black"),
+    panel.border = element_rect(color = "grey20", fill = NA),
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_line(linetype = 3, color = 'gray70'),
     panel.grid.major.y = element_line(linetype = 3, color = 'gray40'),
     panel.grid.minor.y = element_blank(),
 
+    axis.ticks.y = element_line(colour = "grey20"),
+    axis.ticks.length = unit(half_line/4, "pt"),
+
     legend.position = "top",
     legend.justification = c(1, 1),
     legend.direction = "vertical",
     legend.box.background = element_rect(color = "black"),
+    legend.spacing = unit(0.1, "cm"),
 
     plot.title = element_text(size = rel(2)),
     axis.title = element_text(size = rel(1.5)),
@@ -261,7 +267,7 @@ theme_mazamaBar <- function(base_size = 11, base_family = "") {
     axis.text.y = element_text(size = rel(1)),
     strip.text = element_text(size = rel(1.2)),
     legend.title = element_text(size = rel(1)),
-    legend.text = element_text(size = rel(.75))
+    legend.text = element_text(size = rel(.7))
   )
 
 }
