7d4d1d46f32dd9ff92011fc5ba7d1e6ed0e92bd0
Spencer Pease <spencer.p.816@gmail.com>
2018-07-05 12:44:11 -0700

    Update local examples and add function to get plot data

---
+++ localExamples/get_daily_hourly_data.R
@@ -0,0 +1,68 @@
+# convenience function to daily and hourly data
+
+get_daily_hourly_data <- function(monitors, data) {
+
+  monData <- data %>%
+    monitor_subset(monitorIDs = monitors)
+
+  # daily
+  dailyData <- monData %>%
+    monitor_dailyStatistic() %>%
+    monitor_toTidy() %>%
+    mutate(
+      aqiCategory = cut(
+        .data$pm25,
+        AQI$breaks_24,
+        include.lowest = TRUE,
+        labels = AQI$names)) %>%
+    select(datetime, monitorID, pm25, timezone, siteName, aqiCategory)
+
+
+  # nowcast with short term
+  hourlyData_ist <- monData %>%
+    monitor_nowcast(includeShortTerm = TRUE) %>%
+    monitor_toTidy() %>%
+    mutate(
+      aqiCategory = cut(
+        .data$pm25,
+        AQI$breaks_24,
+        include.lowest = TRUE,
+        labels = AQI$names),
+      datetime_tz = lubridate::with_tz(datetime, "America/Los_Angeles")) %>%
+    select(datetime, datetime_tz, monitorID, pm25, timezone, siteName, aqiCategory)
+
+  # nowcast no short term
+  hourlyData_nst <- monData %>%
+    monitor_nowcast(includeShortTerm = FALSE) %>%
+    monitor_toTidy() %>%
+    mutate(
+      aqiCategory = cut(
+        .data$pm25,
+        AQI$breaks_24,
+        include.lowest = TRUE,
+        labels = AQI$names),
+      datetime_tz = lubridate::with_tz(datetime, "America/Los_Angeles")) %>%
+    select(datetime, datetime_tz, monitorID, pm25, timezone, siteName, aqiCategory)
+
+  # raw hourly
+  hourlyData_raw <- monData %>%
+    monitor_toTidy() %>%
+    mutate(
+      aqiCategory = cut(
+        .data$pm25,
+        AQI$breaks_24,
+        include.lowest = TRUE,
+        labels = AQI$names),
+      datetime_tz = lubridate::with_tz(datetime, "America/Los_Angeles")) %>%
+    select(datetime, datetime_tz, monitorID, pm25, timezone, siteName, aqiCategory)
+
+
+  data_list <- list(
+    "hourly_raw" = hourlyData_raw,
+    "hourly_nc_ist" = hourlyData_ist,
+    "hourly_nc_nst" = hourlyData_nst,
+    "daily" = dailyData
+  )
+
+  data_list
+}
--- localExamples/summary_barplot.R
+++ localExamples/summary_barplot.R
@@ -4,23 +4,35 @@
 
 library(PWFSLSmokePlots)
 
+# Get monitors
+
+monitors <- c(
+  #"060798002_01",
+  #"060832004_01",
+  #"060792004_01",
+  "060792006_01",
+  "060831008_01"
+)
+
 # load data
 
 data_latest <- airnow_loadLatest()
 data_2017 <- airnow_load()
+data_2017_sub <- monitor_subset(
+  data_2017,
+  tlim = c(20171201, 20171207),
+  timezone = "America/Los_Angeles"
+)
+data_2017_sub_utc <- monitor_subset(
+  data_2017,
+  tlim = c(20171201, 20171207),
+  timezone = "UTC"
+)
 
-data <- data_latest
-#data <- monitor_subset(data_2017, tlim = c(20170101, 20171231))
+# create plots
 
-monitors <- c(
-  "060831008_01",
-  "060792006_01",
-  "060832004_01",
-  "060798002_01",
-  "060792004_01"
-  )
+p1 <- createTarnayPlot(monitors = monitors, data = data_2017, tlim = c(20171201, 20171207))
+p2 <- createTarnayPlot(monitors = monitors, data = data_2017_sub)
+p2 <- createTarnayPlot(monitors = monitors, data = data_2017_sub_utc)
+p3 <- createTarnayPlot(monitors = monitors, data = data_latest)
 
-# create plot
-
-p <- createTarnayPlot(monitors = monitors, data = data)
-p
