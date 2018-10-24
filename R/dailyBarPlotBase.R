b2a43a9ffc1a1605e2d5ab0f7e349138e762ff4b
Tate Brasel <tate@Glacier.local>
2018-10-23 17:09:03 -0700

    Added more barplot styling

--- R/dailyBarPlotBase.R
+++ R/dailyBarPlotBase.R
@@ -16,15 +16,18 @@
 #' @export
 #' @examples
 #' ws_monitor <- PWFSLSmoke::Carmel_Valley
-#' startdate <- "2016-08-10"
-#' enddate <- "2016-08-17"
+#' startdate <- "2016-08-5"
+#' enddate <- "2016-08-19"
 #' dailyBarPlotBase(ws_monitor, startdate, enddate)
 
 dailyBarPlotBase <- function(ws_monitor,
                              startdate = NULL,
                              enddate = NULL,
                              colorPalette = aqiPalette("aqi"),
-                             title = "") {
+                             title = "",
+                             yLimits = NULL,
+                             barOutlineColor = "black",
+                             barOutlineSize = 1.0) {
   
   # Validate arguments ---------------------------------------------------------
   
@@ -43,6 +46,21 @@ dailyBarPlotBase <- function(ws_monitor,
   }
   
   # Set up style ---------------------------------------------------------------
+  
+  plotTitleSize = 20
+  xAxisTextSize <- 10
+  
+  tickPeriod = "days"
+  dayDiff <- difftime(enddate, startdate, units = "days")
+  if (dayDiff >= 0 && dayDiff <= 7) {
+    tickPeriod = "days"
+  } else if (dayDiff <= 21) {
+    tickPeriod = "3 days"
+  } else if (dayDiff <= 60) {
+    tickPeriod = "weeks"
+  } else {
+    tickPeriod = "months"
+  }
   
   # Time limits ----------------------------------------------------------------
   
@@ -86,53 +104,61 @@ dailyBarPlotBase <- function(ws_monitor,
   # Subset based on startdate and enddate
   mon <- monitor_subset(ws_monitor, tlim=c(startdate,enddate))
   
-  # Solar data -----------------------------------------------------------------
+  # AQIBar data -----------------------------------------------------------------
   
-  ti <- timeInfo(startdate,
-                 mon$meta$longitude,
-                 mon$meta$latitude,
-                 mon$meta$timezone)
+  AQIBar <- data.frame(PWFSLSmoke::AQI$breaks_24)
+  names(AQIBar) <- c("aqi")
   
   # Barplot data ---------------------------------------------------------------
   
-  if (startdate == enddate) {
+  if ( startdate == enddate ) {
     stop("'startdate' and 'enddate' cannot be equal")
   }
   
   dailyData <- PWFSLSmoke::monitor_dailyStatistic(mon)$data
   names(dailyData) <- c("datetime", "pm25")
-  
   dailyData$color = colorPalette(dailyData$pm25)
+  
+  if ( any(is.na(dailyData$pm25)) ) {
+    stop("Missing readings inside date range")
+  }
+  
+  if (is.null(yLimits) ) {
+    yLimits = c(0, max(dailyData$pm25, na.rm = TRUE))
+  }
   
   # Plot data ------------------------------------------------------------------
   
   dailyBarPlotBase <- ggplot() +
   
+    # Add daily statistic bars
     geom_bar(data = dailyData,
              aes(
               x = datetime,
               y = pm25
              ),
              fill = dailyData$color,
-             color = dailyData$color,
+             color = barOutlineColor,
              stat = "identity")
     
-    # Remove all plot decorations
+    # Remove plot decorations
     dailyBarPlotBase <- dailyBarPlotBase +
     theme(panel.background = element_rect(fill = "transparent", color = NA)) +
     theme(plot.background = element_rect(fill = "transparent", color = NA)) +
     theme(panel.grid = element_blank()) +
-    theme(axis.title = element_blank()) +
-    theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 1.0)) + 
-    theme(axis.ticks = element_blank()) +
-    scale_x_datetime(date_breaks = "days" , date_labels = "%b %y")
-
-  
+    theme(axis.title.x = element_blank())
+    
+    # Style text and tickmarks
+    dailyBarPlotBase <- dailyBarPlotBase +
+    theme(axis.text.x = element_text(size = xAxisTextSize, angle = 45, vjust = 0.5)) + 
+    scale_x_datetime(date_breaks = tickPeriod, date_labels = "%b %d") +
+    ylab("PM2.5 (μg/m3)") +
+    ylim(yLimits)
+    
     # Add plot title
     dailyBarPlotBase <- dailyBarPlotBase +
     ggtitle(title) +
-    theme(plot.title = element_text(color = "gray30", size = 20, hjust = 0.5))
-  
+    theme(plot.title = element_text(color = "gray30", size = plotTitleSize, hjust = 0.5))
 
   return(dailyBarPlotBase)
 }
