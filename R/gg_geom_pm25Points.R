191da34d87f9bdffac2e367a2b98e7a1c7d7b8df
helenmiller16 <helen@ohana.org>
2019-01-16 15:06:04 -0800

    Attempt at including legends

--- R/gg_geom_pm25Points.R
+++ R/gg_geom_pm25Points.R
@@ -2,41 +2,45 @@ geom_pm25Points <- function(mapping = NULL,
                             data = NULL,                            
                             position = "identity", 
                             na.rm = FALSE, 
-                            show.legend = NA, 
+                            show.legend = FALSE, 
                             inherit.aes = TRUE, 
                             stat = "identity", 
                             color = "black",
+                            size = 1,
+                            shape = 19,
+                            alpha = 0.3,
                             legend.title = "",
                             legend.label = "PM2.5",
                             ...) {
   
-  # Add mapping to color so legend can be added later if desired
-  print(mapping)
-  if (is.null(mapping)) {
-    mapping <- aes(shape = !!legend.label, linetype = !!legend.label)
-    # Set default shape and linetype
-    pm25_shape <- 19
-    pm25_linetype <- NA
-  } else {
-    pm25_shape <- mapping$shape
-    pm25_linetype <- mapping$linetype
-    mapping$shape <- legend.label
-    mapping$linetype <- legend.label
-  }
+    ## Map aesthetics to a variable (legend.label)
+    if (is.null(mapping)) {
+      mapping <- aes(colour = !!legend.label,
+                     shape = !!legend.label,
+                     size = !!legend.label,
+                     alpha = !!legend.label)
+    } else {
+      mapping$shape <- ifelse(is.null(mapping$shape), legend.label, mapping$shape)
+      mapping$colour <- ifelse(is.null(mapping$colour), legend.label, mapping$colour)
+      mapping$size <- ifelse(is.null(mapping$size), legend.label, mapping$size)
+      mapping$alpha <- ifelse(is.null(mapping$size), legend.label, mapping$size)
+    }
 
+    
+  
   
   list(
     layer(
       stat = stat, data = data, mapping = mapping, geom = GeomPm25Points,
       position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
+      params = list(na.rm = na.rm,
+                    ...)
     ),
-    scale_shape_manual(name = legend.title, 
-                       values = c(legend.label = pm25_shape), 
-                       guide = "legend"),
-    scale_linetype_manual(name = legend.title, 
-                          values = c(legend.label = pm25_linetype), 
-                          guide = "legend")
+      scale_color_manual(name = legend.title, values = color, guide = "legend"),
+      scale_shape_manual(name = legend.title, values = shape, guide = "legend"),
+      scale_alpha_manual(name = legend.title, values = alpha, guide = "legend"),
+      scale_size_manual (name = legend.title, values = size, guide = "legend")
+
     # http://zevross.com/blog/2014/08/04/beautiful-plotting-in-r-a-ggplot2-cheatsheet-3/
     # https://stackoverflow.com/questions/17148679/construct-a-manual-legend-for-a-complicated-plot
   )
